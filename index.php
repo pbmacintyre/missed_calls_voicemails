ac3baffdb3d30c79f047f5948cc5f3954d4a800e
Peter MacIntyre <pbmacintyre@gmail.com>
2024-12-05 16:28:14 -0400

    ready to write the article

--- index.php
+++ index.php
@@ -1,70 +1,80 @@
 <?php
-/**
- * Copyright (C) 2019-2024 Paladin Business Solutions
- */
-ob_start();
-session_start();
 
 require_once('includes/ringcentral-php-functions.inc');
+show_errors();
 
-page_header(0);  // set back to 1 when recaptchas are set in the .ENV file
+require(__DIR__ . '/includes/vendor/autoload.php');
 
-function show_form($message, $label = "", $print_again = false) { ?>
+use RingCentral\SDK\SDK;
 
-    <form action="" method="post">
-        <table class="CustomTable">
-            <tr class="CustomTable">
-                <td colspan="2" class="CustomTableFullCol">
-                    <img src="images/rc-logo.png"/>
-                    <h2><?php app_name(); ?></h2>
-					<?php
-					if ($print_again == true) {
-//                        echo "<p class='msg_bad'>" . $message . "</strong></font>";
-						echo_plain_text($message, "red", "large");
-					} else {
-//                        echo "<p class='msg_good'>" . $message . "</p>";
-						echo_plain_text($message, "#008EC2", "large");
-					} ?>
-                </td>
-            </tr>
-            <tr class="CustomTable">
-                <td colspan="2" class="CustomTableFullCol">
-                    <input type="submit" class="submit_button" value="   Check log   " name="checkLog">
-                </td>
-            </tr>
-            <tr class="CustomTable">
-                <td colspan="2" class="CustomTableFullCol">
-                    <hr>
-                </td>
-            </tr>
-            <tr class="CustomTable">
-                <td colspan="2" class="CustomTableFullCol">
-					<?php app_version(); ?>
-                </td>
-            </tr>
-        </table>
-    </form>
-	<?php
+$dotenv = Dotenv\Dotenv::createMutable(__DIR__ . '/includes')->load();
+
+$client_id = $_ENV['RC_APP_CLIENT_ID'];
+$client_secret = $_ENV['RC_APP_CLIENT_SECRET'];
+
+$jwt_key = $_ENV['RC_JWT_KEY'];
+
+
+$server = 'https://platform.ringcentral.com';
+
+$rcsdk = new SDK($client_id, $client_secret, $server);
+$platform = $rcsdk->platform();
+
+// Authenticate using JWT
+$platform->login(['jwt' => $jwt_key]);
+
+try {
+	// Initialize the RingCentral SDK
+	$rcsdk = new SDK($client_id, $client_secret, $server);
+	$platform = $rcsdk->platform();
+
+	// Authenticate using JWT
+	$platform->login(['jwt' => $jwt_key]);
+
+	// Fetch voicemail messages
+	$response = $platform->get('/restapi/v1.0/account/~/extension/~/message-store', [
+		'messageType' => 'VoiceMail',
+	]);
+
+	$messages = $response->json()->records;
+
+//	echo_spaces("Raw message list", $messages);
+
+	if (!empty($messages)) {
+		foreach ($messages as $message) {
+			if (!empty($message->attachments)) {
+				echo_spaces("Voicemail from ", $message->from->name);
+				echo_spaces("Voicemail # ", $message->from->phoneNumber, 1);
+				foreach ($message->attachments as $attachment) {
+					if ($attachment->contentType == 'audio/mpeg') {
+						$recordingUri = $attachment->uri;
+						// Fetch the recording
+						$recordingResponse = $platform->get($recordingUri);
+						$recordingContent = $recordingResponse->raw();
+						// Save the recording to a local file
+						$fileName = 'voicemail_' . $message->id . '.mp3';
+						file_put_contents($fileName, $recordingContent);
+						// Display a playable link
+						echo "<audio controls>
+                            <source src=\"$fileName\" type=\"audio/mpeg\">
+                            Your browser does not support the audio element.
+                          </audio>";
+					}
+					if ($attachment->contentType == 'text/plain') {
+						$recordingUri = $attachment->uri;
+						$recordingResponse = $platform->get($recordingUri);
+						$recordingContent = $recordingResponse->raw();
+						$fileName = 'transcription_' . $message->id . '.txt';
+						file_put_contents($fileName, $recordingContent);
+						echo_spaces("Transcript", nl2br(htmlspecialchars(file_get_contents($fileName))));
+					}
+				}
+				echo_spaces("===============================","", 1, false);
+			}
+		}
+	} else {
+		echo "No voicemails found.";
+	}
+} catch (Exception $e) {
+	echo 'Error: ' . $e->getMessage();
 }
-
-/* ============= */
-/*  --- MAIN --- */
-/* ============= */
-if (isset($_POST['checkLog'])) {
-	require(__DIR__ . '/includes/vendor/autoload.php');
-	$dotenv = Dotenv\Dotenv::createMutable(__DIR__ . '/includes')->load();
-
-	$client_id = $_ENV['RC_APP_CLIENT_ID'];
-	$redirect_url = $_ENV['RC_REDIRECT_URL'];
-
-    // authorize the access and get an access key
-	$authorization_url = "https://platform.ringcentral.com/restapi/oauth/authorize?response_type=code&client_id={$client_id}&redirect_uri={$redirect_url}";
-
-    header("Location: $authorization_url");
-} else {
-	$message = "Click the Check log button to see if there are any missed calls in the log <br/>";
-	show_form($message);
-}
-
-ob_end_flush();
-page_footer();
