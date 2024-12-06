2f2ca80ccec677087e95d684a5a127e41b30d7e1
Peter MacIntyre <pbmacintyre@gmail.com>
2024-12-06 13:23:55 -0400

    Tidy up code and now ready to write the article

--- index.php
+++ index.php
@@ -1,40 +1,37 @@
 <?php
-
 require_once('includes/ringcentral-php-functions.inc');
-show_errors();
 
-require(__DIR__ . '/includes/vendor/autoload.php');
+// show_errors();
 
-use RingCentral\SDK\SDK;
+require('includes/vendor/autoload.php');
 
 $dotenv = Dotenv\Dotenv::createMutable(__DIR__ . '/includes')->load();
 
 $client_id = $_ENV['RC_APP_CLIENT_ID'];
 $client_secret = $_ENV['RC_APP_CLIENT_SECRET'];
-
 $jwt_key = $_ENV['RC_JWT_KEY'];
-
 
 $server = 'https://platform.ringcentral.com';
 
-$rcsdk = new SDK($client_id, $client_secret, $server);
-$platform = $rcsdk->platform();
-
-// Authenticate using JWT
-$platform->login(['jwt' => $jwt_key]);
-
 try {
 	// Initialize the RingCentral SDK
-	$rcsdk = new SDK($client_id, $client_secret, $server);
+	$rcsdk = new RingCentral\SDK\SDK($client_id, $client_secret, $server);
 	$platform = $rcsdk->platform();
 
 	// Authenticate using JWT
 	$platform->login(['jwt' => $jwt_key]);
 
-	// Fetch voicemail messages
-	$response = $platform->get('/restapi/v1.0/account/~/extension/~/message-store', [
+	$startDate = date('Y-m-d\TH:i:s\Z', strtotime('-2 weeks'));
+	$endDate = date('Y-m-d\TH:i:s\Z', strtotime('now'));
+
+	$queryParams = array(
+		'dateFrom' => $startDate,
+		'dateTo' => $endDate,
 		'messageType' => 'VoiceMail',
-	]);
+	);
+
+	// Fetch voicemail messages
+	$response = $platform->get('/restapi/v1.0/account/~/extension/~/message-store', $queryParams);
 
 	$messages = $response->json()->records;
 
@@ -43,6 +40,7 @@ try {
 	if (!empty($messages)) {
 		foreach ($messages as $message) {
 			if (!empty($message->attachments)) {
+				echo_spaces("========== Voice Mail Information =====================","", 1, false);
 				echo_spaces("Voicemail from ", $message->from->name);
 				echo_spaces("Voicemail # ", $message->from->phoneNumber, 1);
 				foreach ($message->attachments as $attachment) {
@@ -56,20 +54,17 @@ try {
 						file_put_contents($fileName, $recordingContent);
 						// Display a playable link
 						echo "<audio controls>
-                            <source src=\"$fileName\" type=\"audio/mpeg\">
-                            Your browser does not support the audio element.
-                          </audio>";
+                            <source src=\"$fileName\" type=\"audio/mpeg\">                            
+                          </audio><br/>";
+						echo_spaces("", "", 0, false);
 					}
 					if ($attachment->contentType == 'text/plain') {
 						$recordingUri = $attachment->uri;
 						$recordingResponse = $platform->get($recordingUri);
 						$recordingContent = $recordingResponse->raw();
-						$fileName = 'transcription_' . $message->id . '.txt';
-						file_put_contents($fileName, $recordingContent);
-						echo_spaces("Transcript", nl2br(htmlspecialchars(file_get_contents($fileName))));
+						echo $recordingContent . "<br/>" ;
 					}
 				}
-				echo_spaces("===============================","", 1, false);
 			}
 		}
 	} else {
